5dae869176fc6f040bce67ca2b861a14cf7dfc26
philvilla25 <114821553+philvilla25@users.noreply.github.com>
2024-07-14 16:41:31 -0400

    lab2

---
+++ lab2version2/src/main/java/cst8218/vill0419/lab2version2/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package cst8218.vill0419.lab2version2.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
